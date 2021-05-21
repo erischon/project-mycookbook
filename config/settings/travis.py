5eab5f59d3016d3d32a05ee000e11c4beff3652f
Eri Schon <erischon@gmail.com>
2021-05-21 13:44:19 +0200

    CI/CD with travis

---
+++ config/settings/travis.py
@@ -0,0 +1,15 @@
+from . import *
+
+SECRET_KEY = ("change_me")
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
