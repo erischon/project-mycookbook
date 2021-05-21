bdf6b9c4502bb9dc413114066036ab312b05af6b
Eri Schon <erischon@gmail.com>
2021-05-21 16:56:48 +0200

    Update __init__.py

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY')
