641afe74e1114142e4ec3d127581a313592d9c9e
Eri Schon <erischon@gmail.com>
2021-05-21 16:45:57 +0200

    Update __init__.py

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -105,6 +105,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 
