ba21c7cb7f2eee6e4aa453373188d7abc0719b7f
Eri Schon <erischon@gmail.com>
2021-05-30 11:37:22 +0200

    frontend signup page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import gettext as _
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username',)
+        fields = ('email', 'username', )
+
+        labels = {
+            'username': 'Prénom :',
+            'email': 'Adresse mail :',
+            'password1': 'Mot de passe :',
+            'password2': 'Confirmez votre mot de passe :'
+        }
 
 
 class CustomUserChangeForm(UserChangeForm):
--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd
+
     # Local
     'core',
     'accounts',
