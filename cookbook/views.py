edd8eab118a20ca3d8291dcc4d6d80d3372b0c17
Eri Schon <erischon@gmail.com>
2021-06-01 12:12:39 +0200

    refactoring

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+
 # from django.http import HttpResponseRedirect
 
 from .forms import CookbookCreationForm
 from .models import Cookbook
 
 
-def cookbook_creation(request):
-    return render(request, 'cookbook/cookbook-create.html')
-
-
+@login_required
 def recipe_creation(request):
     return render(request, 'cookbook/recipe-create.html')
 
 
+@login_required
 def create_cookbook(request):
     if request.method == 'POST':
         form = CookbookCreationForm(request.POST)
--- core/views.py
+++ core/views.py
@@ -7,10 +7,12 @@ from cookbook.models import Cookbook
 def homePage(request):
     return render(request, 'core/home.html')
 
+
 @login_required
 def user_admin(request):
     cookbook = Cookbook.objects.get(user=request.user)
     return render(request, 'core/user_admin.html', {'cookbook': cookbook})
+
 
 @login_required
 def my_cookbook(request):
