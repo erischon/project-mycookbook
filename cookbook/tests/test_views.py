ad7c542063d8eb2f6add082139da31a2be2e1b2f
Eri Schon <erischon@gmail.com>
2021-06-07 11:10:42 +0200

    create recipe view post testing

--- cookbook/tests/test_views.py
+++ cookbook/tests/test_views.py
@@ -1,33 +1,50 @@
-from cookbook.models import Cookbook, Recipe
 from django.http import request
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+from cookbook.forms import RecipeCreationForm
+from cookbook.models import Cookbook, Recipe, RecipeInfos
 
-class CookbookTestsViews(TestCase):
+
+class CookbookTestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.client_b = Client()
+        # Users
         self.user = get_user_model().objects.create_user(
             username='usertest',
             email='usertest@erischon.dev',
             password='testpass123'
         )
+        self.user_b = get_user_model().objects.create_user(
+            username='user test b',
+            email='usertestb@erischon.dev',
+            password='testpass123'
+        )
         self.client.force_login(self.user)
+        self.client_b.force_login(self.user_b)
 
-        # self.cookbook = Cookbook.objects.create(
-        #     name='Cookbook test',
-        #     user=self.user,
-        # )
-
+        # Cookbook
+        self.cookbook = Cookbook.objects.create(
+            name='Cookbook test',
+            user=self.user_b,
+        )
+        # Recipe
         self.recipe = Recipe.objects.create (
             title='Recette test',
-            description='',
+            description='Une description Test',
             guest=3,
-            prep_time='0:0:0',
-            cook_time='0:0:0',
+            prep_time='0:5:0',
+            cook_time='0:5:0',
             source='Aucune',
+        )
+        self.recipeinfos = RecipeInfos.objects.create (
+            creator=self.user,
+            owner=self.user,
+            slug='recette-test',
+            recipe=self.recipe,
         )
 
         self.cb_create_url = reverse('cookbook_create')
@@ -44,7 +61,7 @@ class CookbookTestsViews(TestCase):
     def test_cookbook_creation(self):
         """ """
         cb_name = 'a cookbook name'
-        query = {'name': cb_name, 'user': self.client}
+        query = {'name': cb_name, 'user': self.client_b}
 
         response = self.client.post(self.cb_create_url, query)
         cookbook_name = Cookbook.objects.get(name=cb_name)
@@ -60,13 +77,44 @@ class CookbookTestsViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'cookbook/recipe-create.html')
 
-    def test_cookbook_recipe_page_view(self):
+    def test_cookbook_recipe_detail_page_view(self):
         """ """
         response = self.client.get(self.recipe_page_url)
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'cookbook/recipe-detail.html')
 
-    def test_recipe_creation(self):
+
+class CreateRecipeTestPostView(TestCase):
+
+    # l'utilisateur doit être log
+    # valider la method POST
+    # valider qu'il est bien redirigé vers home
+
+    def setUp(self):
+        self.client = Client()
+        # Users
+        self.user = get_user_model().objects.create_user(
+            username='usertest',
+            email='usertest@erischon.dev',
+            password='testpass123'
+        )
+        self.client.force_login(self.user)
+        self.cookbook = Cookbook.objects.create(
+            name='Cookbook test',
+            user=self.user,
+        )
+
+        self.recipe_create_url = reverse('recipe_create')
+
+    def test_recipe_creation_POST(self):
         """ Test the recipe creation in database. """
-        pass
+        response = self.client.post(self.recipe_create_url, {
+            'recipe_form': 'True', 'title': '', 'description': '', 'guest': '', 'prep_time': '', 'cook_time': '', 'source': '',
+            'ingredient_formset': 'True', 'name': '', 'measure': '', 'quantity': '',
+            'instruction_formset': 'True', 'step': '', 'instruction': '',
+             'tag_formset': 'True', 'name': '', 'tagtype': '',
+        })
+
+        self.assertEquals(response.status_code, 302)
+        # self.assertTemplateUsed(response, 'cookbook/recipe-create.html')
