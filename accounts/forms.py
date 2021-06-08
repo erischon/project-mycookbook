b0d1e6d1e7ce4e4d2b05000f507fe0ca3e17e1f2
Eri Schon <erischon@gmail.com>
2021-06-08 14:05:00 +0200

    frontend login page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django.utils.translation import gettext as _
 
 
