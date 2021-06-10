c41dd6cc8ef71dc90b5b6c857db5ad1aed67194e
Eri Schon <erischon@gmail.com>
2021-06-10 18:07:15 +0200

    Update urls.py

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('cookbook-create/', views.create_cookbook, name='cookbook_create'),
     path('recipe-create/', views.create_recipe, name='recipe_create'),
     path('recipe-detail/<slug:pk>', login_required(RecipeDetailView.as_view()), name='recipe-detail'),
-    path('recipe-detail/<slug:pk>/edit', login_required(RecipeEditView.as_view()), name='recipe-edit'),
-    path('recipe-detail/<slug:pk>/edit/ingredient', login_required(IngredientEditView.as_view()), name='ingredient-edit'),
-    # path('recipe-detail/<slug:pk>/edit', views.update_recipe, name='recipe-edit'),
+    # path('recipe-detail/<slug:pk>/edit', login_required(RecipeEditView.as_view()), name='recipe-edit'),
+    # path('recipe-detail/<slug:pk>/edit/ingredient', login_required(IngredientEditView.as_view()), name='ingredient-edit'),
+    path('recipe-detail/<slug:pk>/edit', views.update_recipe, name='recipe-edit'),
 ]
