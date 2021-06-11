7c4f3cfe699dbb1827f8b61d755ef959dc408978
Eri Schon <erischon@gmail.com>
2021-06-11 11:31:21 +0200

    recipe crud

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from .views import RecipeDetailView
+from .views import RecipeDetailView, RecipeEditView, RecipeDeleteView, IngredientEditView, IngredientAddView, InstructionEditView, InstructionAddView, TagEditView, TagAddView
 from . import views
 
 
@@ -9,5 +9,12 @@ urlpatterns = [
     path('cookbook-create/', views.create_cookbook, name='cookbook_create'),
     path('recipe-create/', views.create_recipe, name='recipe_create'),
     path('recipe-detail/<slug:pk>', login_required(RecipeDetailView.as_view()), name='recipe-detail'),
-    path('recipe-detail/<slug:pk>/edit', views.update_recipe, name='recipe-edit'),
+    path('<slug:pk>/recipe-edit', login_required(RecipeEditView.as_view()), name='recipe-edit'),
+    path('<slug:pk>/recipe-delete', login_required(RecipeDeleteView.as_view()), name='recipe-delete'),
+    path('<int:pk>/ingredient-edit', login_required(IngredientEditView.as_view()), name='ingredient-edit'),
+    path('<int:pk>/ingredient-add', login_required(IngredientAddView.as_view()), name='ingredient-add'),
+    path('<int:pk>/instruction-edit', login_required(InstructionEditView.as_view()), name='instruction-edit'),
+    path('<int:pk>/instruction-add', login_required(InstructionAddView.as_view()), name='instruction-add'),
+    path('<int:pk>/tag-edit', login_required(TagEditView.as_view()), name='tag-edit'),
+    path('<int:pk>/tag-add', login_required(TagAddView.as_view()), name='tag-add'),
 ]
--- cookbook/views.py
+++ cookbook/views.py
@@ -2,11 +2,13 @@ from django.db.models import query
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.forms import ModelForm, modelformset_factory
+from django.urls.base import reverse_lazy
 from django.utils.text import slugify
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView, CreateView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 from .forms import CookbookCreationForm, RecipeCreationForm, IngredientForm, InstructionForm, TagForm
@@ -93,60 +95,6 @@ def create_recipe(request):
     return render(request, 'cookbook/recipe-create.html', context)
 
 
-@login_required
-def update_recipe(request, pk):
-    # Formsets
-    IngredientFormSet = modelformset_factory(Ingredient, form=IngredientForm, extra=0)
-    InstructionFormSet = modelformset_factory(Instruction, form=InstructionForm, extra=0)
-    TagFormSet = modelformset_factory(Tag, form=TagForm, extra=0)
-    # Query
-    recipe = Recipe.objects.get(id=pk)
-    ingredient_queryset = Ingredient.objects.filter(recipe=recipe)
-    instruction_queryset = Instruction.objects.filter(recipe=recipe)
-    tag_queryset = Tag.objects.filter(recipe=recipe)
-    # Form
-    recipe_form = RecipeCreationForm(request.POST, instance=recipe)
-    ingredient_formset = IngredientFormSet(request.POST, prefix='ingredient', queryset=ingredient_queryset)
-    instruction_formset = InstructionFormSet(request.POST, prefix='instruction', queryset=instruction_queryset)
-    tag_formset = TagFormSet(request.POST, prefix='tag', queryset=tag_queryset)
-
-    if recipe_form.is_valid() and ingredient_formset.is_valid() and instruction_formset.is_valid():
-        # Recipe
-        recipe = recipe_form.save(commit=False)
-        recipe.save()
-        recipe.cookbook.set([Cookbook.objects.get(user=request.user)])
-        # Recipe Infos
-        creator = request.user
-        owner = request.user
-        slug = slugify(recipe.title)
-        RecipeInfos.objects.create(creator=creator, owner=owner, slug=slug, recipe=recipe)
-        # Ingredients
-        ingredients = ingredient_formset.save(commit=False)
-        for ingredient in ingredients:
-            ingredient.recipe = recipe
-            ingredient.save()
-        # Instructions
-        instructions = instruction_formset.save(commit=False)
-        for instruction in instructions:
-            instruction.recipe = recipe
-            instruction.save()
-        # Tags
-        tags = tag_formset.save(commit=False)
-        for tag in tags:
-            tag.recipe = recipe
-            tag.save()
-
-        return redirect('my_cookbook')
-
-    context = {
-        'recipe_form': recipe_form,
-        'ingredient_formset': ingredient_formset,
-        'instruction_formset': instruction_formset,
-        'tag_formset': tag_formset,
-    }
-    return render(request, 'cookbook/recipe-create.html', context)
-
-
 class RecipeDetailView(DetailView):
 
     model = Recipe
@@ -162,3 +110,83 @@ class RecipeDetailView(DetailView):
         context['tags'] = Tag.objects.filter(recipe=context['object'])
 
         return context
+
+
+class RecipeEditView(UpdateView):
+    model = Recipe
+    form_class = RecipeCreationForm
+    template_name = 'cookbook/recipe-edit.html'
+
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', args=(self.object.id,))
+
+
+class IngredientEditView(UpdateView):
+    model = Ingredient
+    form_class = IngredientForm
+    template_name = 'cookbook/ingredient-edit.html'
+
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', args=(self.object.recipe.id,))
+
+
+class IngredientAddView(CreateView):
+    model = Ingredient
+    form_class = IngredientForm
+    template_name = 'cookbook/ingredient-edit.html'
+
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs.get('pk'))
+        return super(IngredientAddView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', args=(self.object.recipe.id,))
+
+
+class InstructionEditView(UpdateView):
+    model = Instruction
+    form_class = InstructionForm
+    template_name = 'cookbook/instruction-edit.html'
+
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', args=(self.object.recipe.id,))
+
+
+class InstructionAddView(CreateView):
+    model = Instruction
+    form_class = InstructionForm
+    template_name = 'cookbook/instruction-edit.html'
+
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs.get('pk'))
+        return super(InstructionAddView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', args=(self.object.recipe.id,))
+
+
+class TagEditView(UpdateView):
+    model = Tag
+    form_class = TagForm
+    template_name = 'cookbook/tag-edit.html'
+
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', args=(self.object.recipe.id,))
+
+
+class TagAddView(CreateView):
+    model = Tag
+    form_class = TagForm
+    template_name = 'cookbook/tag-edit.html'
+
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs.get('pk'))
+        return super(TagAddView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', args=(self.object.recipe.id,))
+
+
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    success_url = '/mycookbook/'
