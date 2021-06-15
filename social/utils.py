393c59524ff2b7254aa56293668c4d8640a35cd7
Eri Schon <erischon@gmail.com>
2021-06-15 09:39:21 +0200

    Create utils.py

---
+++ social/utils.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+
+def random_string(string_length):
+    """ Generate a random string of fixed length. """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(string_length))
+
+
+def increment_by_one(link):
+    link.use_count = link.use_count + 1
+    link.save()
+    return link
