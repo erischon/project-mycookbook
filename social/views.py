c27dadbe8941699d07b27b39e78f2bd65998bd77
Eri Schon <erischon@gmail.com>
2021-06-15 09:30:27 +0200

    Update views.py

--- social/views.py
+++ social/views.py
@@ -12,7 +12,7 @@ STRING_LENGTH = 20
 MAX_IMPRESSION = 3
 
 
-def randomString(string_length):
+def random_string(string_length):
     """ Generate a random string of fixed length. """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(string_length))
@@ -20,7 +20,7 @@ def randomString(string_length):
 
 def generate_link(request, id):
     """ Generate the link itself. """
-    the_string = randomString(STRING_LENGTH)
+    the_string = random_string(STRING_LENGTH)
     OneTimeLinkModel.objects.create(one_time_code=the_string)
     return HttpResponse('<a href="/social/otl/{}/{}">{}{}</a>'.format(id, the_string, request.build_absolute_uri(), the_string))
 
@@ -28,11 +28,9 @@ def generate_link(request, id):
 def one_time_link(request, id, access_code):
     """ Handles the link request. """
     if OneTimeLinkModel.objects.filter(one_time_code=access_code).exists():
-        model = OneTimeLinkModel.objects.get(one_time_code=access_code)
-        model.use_count = model.use_count + 1
-        model.save()
+        link = increment_by_one(OneTimeLinkModel.objects.get(one_time_code=access_code))
 
-        if model.use_count <= MAX_IMPRESSION:
+        if link.use_count <= MAX_IMPRESSION:
             url = reverse('social-detail', args=[id])
             return redirect(url)
         else:
@@ -43,3 +41,9 @@ def one_time_link(request, id, access_code):
         return HttpResponse("Bad or expired link.")
     else:
         return HttpResponse("Bad or expired link.")
+
+
+def increment_by_one(link):
+    link.use_count = link.use_count + 1
+    link.save()
+    return link
