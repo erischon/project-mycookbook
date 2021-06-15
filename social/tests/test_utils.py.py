301d001468dd2064770bd1d1f6fbbea2ce582bf7
Eri Schon <erischon@gmail.com>
2021-06-15 09:48:01 +0200

    Create test_utils.py.py

---
+++ social/tests/test_utils.py.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+
+class SocialTestsUtils(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_random_string(self):
+        """ Test the random_string method. """
+        pass
+
+    def test_increment_by_one(self):
+        """ Test the increment_by_one method. """
+        pass
