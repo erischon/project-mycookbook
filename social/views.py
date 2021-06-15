9394bd04a5d3fd76a18c49fdb0a08a88989e8db0
Eri Schon <erischon@gmail.com>
2021-06-15 09:19:39 +0200

    cleaning

--- social/views.py
+++ social/views.py
@@ -1,8 +1,5 @@
 import random
 import string
-import datetime
-from datetime import date
-from datetime import timedelta
 
 from django.shortcuts import redirect
 from django.http import HttpResponse
@@ -11,15 +8,19 @@ from django.urls import reverse
 from .models import OneTimeLinkModel
 
 
-def randomString(stringLength):
+STRING_LENGTH = 20
+MAX_IMPRESSION = 3
+
+
+def randomString(string_length):
     """ Generate a random string of fixed length. """
     letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
+    return ''.join(random.choice(letters) for i in range(string_length))
 
 
 def generate_link(request, id):
     """ Generate the link itself. """
-    the_string = randomString(20)
+    the_string = randomString(STRING_LENGTH)
     OneTimeLinkModel.objects.create(one_time_code=the_string)
     return HttpResponse('<a href="/social/otl/{}/{}">{}{}</a>'.format(id, the_string, request.build_absolute_uri(), the_string))
 
@@ -31,7 +32,7 @@ def one_time_link(request, id, access_code):
         model.use_count = model.use_count + 1
         model.save()
 
-        if model.use_count <= 3:
+        if model.use_count <= MAX_IMPRESSION:
             url = reverse('social-detail', args=[id])
             return redirect(url)
         else:
