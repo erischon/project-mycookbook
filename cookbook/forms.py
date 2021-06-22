ea132ad419f71090ddbd183d11b43a774f9c4bd1
Eri Schon <erischon@gmail.com>
2021-06-22 10:09:07 +0200

    0.1.2-a.1-005 | edit form design

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -27,13 +27,13 @@ class RecipeCreationForm(ModelForm):
             'source': 'Source de la recette'
         }
         widgets = {
-            'title': TextInput(attrs={'placeholder': 'Nom de la recette', 'class': 'bg-gray-50 p-3 mb-4 w-full sm:w-1/2 shadow-md border-0 rounded'}),
-            'description': Textarea(attrs={'placeholder': 'Une description', 'class': 'bg-gray-50 p-3 mb-4 w-full sm:w-1/2 shadow-md rounded', 'rows': 3}),
-            'recipe_type': Select(attrs={'class': 'bg-gray-50 p-3 mb-4 w-full sm:w-1/3 shadow-md border-0 rounded'}),
-            'guest': TextInput(attrs={'placeholder': 'Pour combien de personne ?', 'class': 'bg-gray-50 p-3 mb-4 w-full sm:w-1/2 shadow-md border-0 rounded'}),
-            'prep_time': TextInput(attrs={'placeholder': 'Temps de préparation (heures:minutes)', 'class': 'bg-gray-50 p-3 mb-4 w-full sm:w-1/2 shadow-md border-0 rounded'}),
-            'cook_time': TextInput(attrs={'placeholder': 'Temps de cuisson (heures:minutes)', 'class': 'bg-gray-50 p-3 mb-4 w-full sm:w-1/2 shadow-md border-0 rounded'}),
-            'source': TextInput(attrs={'placeholder': 'Source', 'class': 'bg-gray-50 p-3 mb-4 w-full sm:w-1/2 shadow-md border-0 rounded'}),
+            'title': TextInput(attrs={'placeholder': 'Nom de la recette', 'class': 'bg-gray-50 p-3 mb-4 w-full shadow-md border-0 rounded'}),
+            'description': Textarea(attrs={'placeholder': 'Une description', 'class': 'bg-gray-50 p-3 mb-4 w-full shadow-md rounded', 'rows': 3}),
+            'recipe_type': Select(attrs={'class': 'bg-gray-50 p-3 mb-4 w-full shadow-md border-0 rounded'}),
+            'guest': TextInput(attrs={'placeholder': 'Pour combien de personne ?', 'class': 'bg-gray-50 p-3 mb-4 w-full shadow-md border-0 rounded'}),
+            'prep_time': TextInput(attrs={'placeholder': 'Temps de préparation (heures:minutes)', 'class': 'bg-gray-50 p-3 mb-4 w-full shadow-md border-0 rounded'}),
+            'cook_time': TextInput(attrs={'placeholder': 'Temps de cuisson (heures:minutes)', 'class': 'bg-gray-50 p-3 mb-4 w-full shadow-md border-0 rounded'}),
+            'source': TextInput(attrs={'placeholder': 'Source', 'class': 'bg-gray-50 p-3 mb-4 w-full shadow-md border-0 rounded'}),
         }
 
 
