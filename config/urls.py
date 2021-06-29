de77f6cb17c6fb2a78095e599b6d2bb0100a5ea4
Eri Schon <erischon@gmail.com>
2021-06-29 10:33:31 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('accounts/', include('django.contrib.auth.urls')),
-
     path('', include('core.urls')),
     path('accounts/', include('accounts.urls')),
     path('cookbook/', include('cookbook.urls')),
     path('private/', include('private.urls')),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
